05e0db3128b5224354d6a32ce84f3dcda3ee9045
garima2004yadav <115277218+garima2004yadav@users.noreply.github.com>
2023-10-17 22:24:59 +0530

    Add files via upload

---
+++ Result.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Result {
+    public static void main(string[]args){
+        Scanner sc = new scanner(System.in);
+        System.out.println("Enter your name");
+        String name = sc.nextLine();
+        System.out.println("Enter your clas");
+        int clas = sc.nextInt();
+        System.out.println("Enter your maths marks");
+        float mathsmarks = sc.nextfloat();
+        System.out.println("Enter your hindi marks");
+        float hindimarks = sc.nextfloat();
+        System.out.println("Enter your physics marks");
+        float physicsmarks = sc.nextfloat();
+        System.out.println("Enter your chemistry marks");
+        float chemistrymarks = sc.nextfloat();
+        System.out.println("Enter your biology marks");
+        float biologymarks = sc.nextfloat();
+        float TotalAvgMarks = (maths + hindi + physics + chemistry + biology)/5;
+        float percentage = (maths = hindi + physics + chemistry + biology)*100/500;
+
+
+    }
+    
+}
